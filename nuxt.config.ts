64c52567c42645fd5676fd485363313534f9fa22
modalsoul0226 <inpraiseofdionysus@proton.me>
2025-01-04 10:48:21 -0500

    chore: setup vite+nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	devServer: {
+		host: '127.0.0.1',
+		port: 3000
+	}
+})
